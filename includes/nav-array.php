0585d65b6eb1efe6f5b93a720b9f54fc8f699ff6
Amy Rosamond <amyrosamond@Amys-MacBook-Pro.local>
2025-08-07 21:26:35 +0100

    footer fixes

--- includes/nav-array.php
+++ includes/nav-array.php
@@ -67,19 +67,26 @@ $navBarFooter[2] = array(
 );
 
 $navBarFooter[3] = array(
-    "icon" => '<i class="bx bx-phone-call" ></i>',
+    "icon" => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M14.25 9.75v-4.5m0 4.5h4.5m-4.5 0 6-6m-3 18c-8.284 0-15-6.716-15-15V4.5A2.25 2.25 0 0 1 4.5 2.25h1.372c.516 0 .966.351 1.091.852l1.106 4.423c.11.44-.054.902-.417 1.173l-1.293.97a1.062 1.062 0 0 0-.38 1.21 12.035 12.035 0 0 0 7.143 7.143c.441.162.928-.004 1.21-.38l.97-1.293a1.125 1.125 0 0 1 1.173-.417l4.423 1.106c.5.125.852.575.852 1.091V19.5a2.25 2.25 0 0 1-2.25 2.25h-2.25Z" />
+    </svg>',
     "href" => 'tel:' . Baseit::TEL->value . '',
     "name" => 'Phone Us'
 );
 
 $navBarFooter[4] = array(
-    "icon" => '<i class="bx bx-envelope" ></i>',
+    "icon" => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M21.75 6.75v10.5a2.25 2.25 0 0 1-2.25 2.25h-15a2.25 2.25 0 0 1-2.25-2.25V6.75m19.5 0A2.25 2.25 0 0 0 19.5 4.5h-15a2.25 2.25 0 0 0-2.25 2.25m19.5 0v.243a2.25 2.25 0 0 1-1.07 1.916l-7.5 4.615a2.25 2.25 0 0 1-2.36 0L3.32 8.91a2.25 2.25 0 0 1-1.07-1.916V6.75" />
+    </svg>',
     "href" => 'mailto:' . Baseit::EMAIL->value . '',
     "name" => 'Email Us'
 );
 
 $navBarFooter[5] = array(
-    "icon" => '<i class="bx bx-map-pin" ></i>',
+    "icon" => '<svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke-width="1.5" stroke="currentColor" class="size-6">
+    <path stroke-linecap="round" stroke-linejoin="round" d="M15 10.5a3 3 0 1 1-6 0 3 3 0 0 1 6 0Z" />
+    <path stroke-linecap="round" stroke-linejoin="round" d="M19.5 10.5c0 7.142-7.5 11.25-7.5 11.25S4.5 17.642 4.5 10.5a7.5 7.5 0 1 1 15 0Z" />
+    </svg>',
     "href" => '#',
     "name" => 'Locate Us'
 );
@@ -87,11 +94,11 @@ $navBarFooter[5] = array(
 $navBarFooterHours = array();
 
 $navBarFooterHours[0] = array(
-    "monday" => '<strong>Monday</strong> <span>07:00 - 17:00</span>',
-    "tuesday" => '<strong>Tuesday</strong> <span>07:00 - 17:00</span>',
-    "wednesday" => '<strong>Wednesday</strong> <span>07:00 - 17:00</span>',
-    "thursday" => '<strong>Thursday</strong> <span>07:00 - 17:00</span>',
-    "friday" => '<strong>Friday</strong> <span>07:00 - 17:00</span>',
-    "saturday" => '<strong>Saturday</strong> <span>10:00 - 16:00</span>',
-    "sunday" => '<strong>Sunday</strong> <span>Closed</span>'
+    "monday" => '<strong><span>M</span>onday</strong> <span>07:00 - 17:00</span>',
+    "tuesday" => '<strong><span>T</span>uesday</strong> <span>07:00 - 17:00</span>',
+    "wednesday" => '<strong><span>W</span>ednesday</strong> <span>07:00 - 17:00</span>',
+    "thursday" => '<strong><span>T</span>hursday</strong> <span>07:00 - 17:00</span>',
+    "friday" => '<strong><span>F</span>riday</strong> <span>07:00 - 17:00</span>',
+    "saturday" => '<strong><span>S</span>aturday</strong> <span>10:00 - 16:00</span>',
+    "sunday" => '<strong><span>S</span>unday</strong> <span>Closed</span>'
 );
